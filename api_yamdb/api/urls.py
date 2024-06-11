b2394b0c54bb70b1666a992ee60ce358751a8a35
Tatiana Duplinskaia <tancher5@gmail.com>
2024-06-11 19:46:09 +0300

    Add user registration

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,20 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
+from api.views import (CategoryViewSet,
+                       CreateOrListUsersByAdminViewSet,
+                       GenreViewSet,
+                       TitleViewSet,
+                       UserSignupView)
 
 
 router_v1 = routers.DefaultRouter()
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('users', CreateOrListUsersByAdminViewSet, basename='users')
 
 urlpatterns = [
+    path('v1/auth/signup/', UserSignupView.as_view(), name='signup'),
     path('v1/', include(router_v1.urls)),
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from datetime import timedelta
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,3 +110,16 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 AUTH_USER_MODEL = 'users.MdbUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
