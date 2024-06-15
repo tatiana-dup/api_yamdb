24e0927f4dc1ecea1df525dc1599f869841b0cbd
Tatiana Duplinskaia <tancher5@gmail.com>
2024-06-15 22:14:21 +0300

    Refactor confirmation code

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,11 +1,11 @@
-import jwt
-from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from reviews.const import MAX_SCORE_VALUE, MIN_SCORE_VALUE
+from api.utils import send_conform_mail
 from reviews.models import (
     Category,
     Comment,
@@ -28,6 +28,34 @@ class SignupSerializer(serializers.Serializer):
     username = serializers.CharField(
         max_length=USERNAME_MAX_LENGTH,
         validators=[validate_username])
+
+    def validate(self, data):
+        email = data.get('email')
+        username = data.get('username')
+
+        user_by_email = User.objects.filter(email=email).first()
+        user_by_username = User.objects.filter(username=username).first()
+
+        errors = {}
+        if user_by_email != user_by_username:
+            if user_by_email:
+                errors['email'] = [
+                    'Этот емейл используется с другим юзернейм.']
+            if user_by_username:
+                errors['username'] = [
+                    f'Юзернейм {username} занят другим пользователем.']
+            raise ValidationError(errors)
+
+        data['user'] = user_by_email
+        return data
+
+    def create(self, validated_data):
+        user = (validated_data.get('user')
+                or User.objects.create(
+                    email=validated_data.get('email'),
+                    username=validated_data.get('username')))
+        send_conform_mail(user)
+        return user
 
 
 class UsersSerializer(serializers.ModelSerializer):
@@ -53,28 +81,14 @@ class ObtainTokenSerializer(serializers.Serializer):
     username = serializers.CharField()
     confirmation_code = serializers.CharField()
 
-    def validate_username(self, value):
-        self.user = get_object_or_404(User, username=value)
-        return value
-
-    def validate_confirmation_code(self, value):
-        try:
-            payload = jwt.decode(
-                value, settings.SECRET_KEY, algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            raise serializers.ValidationError('Код подтверждения истек.')
-        except jwt.InvalidTokenError:
-            raise serializers.ValidationError(
-                'Недействительный код подтверждения.')
-
-        self.username = payload.get('username')
-        return value
-
     def validate(self, data):
-        if data.get('username') != self.username:
+        user = get_object_or_404(User, username=data['username'])
+        is_confirmation_code_correct = default_token_generator.check_token(
+            user, data['confirmation_code'])
+        if not is_confirmation_code_correct:
             raise serializers.ValidationError(
-                'Неверный код подтверждения или юзернейм.')
-        data['user'] = self.user
+                'Недействительный или истекший код подтверждения.')
+        data['user'] = user
         return data
 
     def get_token_for_user(self, user):
