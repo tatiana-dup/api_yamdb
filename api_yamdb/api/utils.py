fe25323cd5ade48a5b243cde0681bdb72c7c8b4d
Tatiana Duplinskaia <tancher5@gmail.com>
2024-06-16 10:35:48 +0300

    Add defoult email

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 
+from api_yamdb.settings import DEFAULT_FROM_EMAIL
+
 
 def generate_confirmation_code(user):
     confirmation_code = default_token_generator.make_token(user)
@@ -11,8 +13,7 @@ def send_conform_mail(user):
     confirmation_code = generate_confirmation_code(user)
     subject = 'Ваш код подтверждения'
     message = f'Ваш код подтверждения {confirmation_code}'
-    from_email = 'yamdb@example.com'
     recipient_list = [user.email]
 
-    send_mail(subject, message, from_email, recipient_list,
+    send_mail(subject, message, DEFAULT_FROM_EMAIL, recipient_list,
               fail_silently=False)
